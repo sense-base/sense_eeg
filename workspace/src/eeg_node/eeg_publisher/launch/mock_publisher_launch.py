0f076fa24ae9a5746fe0c8d5639554b0cec9ad23
nikk-nikaznan <48319650+nikk-nikaznan@users.noreply.github.com>
2025-04-08 17:37:24 +0100

    Mock EEG publisher and EEG msg (#2)
    * add gitignore
    
    * add the workspace
    
    * add launch
    
    * cleaned up newline
    
    * moved the readme

---
+++ workspace/src/eeg_node/eeg_publisher/launch/mock_publisher_launch.py
@@ -0,0 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='eeg_publisher',
+            namespace='eeg_publisher1',
+            executable='mock_publisher',
+            name='mock_publisher'
+        )
+    ])
