c9658faca7674ad8d0abfdb6075415a8dab42f72
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2025-04-11 12:01:42 +0100

    adds  seed to make data deterministic; adds self variables in the __init__; updates message types to uint8 (max 255 values); and adds ros bag instructions n the main README #1

--- README.md
+++ README.md
@@ -5,11 +5,10 @@
 
 ## :eyeglasses: Overview
 This repository contains documentation and code for EEG packages. 
-
 ## :school_satchel: Getting started
 * :computer: [Setting up ROS2 with docker container](https://github.com/sense-base/base/tree/main/docs/docker)
 
-* :octocat: Clone repo under and refer to the [CONTRIBUTING](CONTRIBUTING.md) guideline for detailed instructions on contributing to this repo.
+* :octocat: Clone repo under `sense-base` path and refer to the [CONTRIBUTING](CONTRIBUTING.md) guideline for detailed instructions on contributing to this repo.
 ```
 git clone git@github.com:sense-base/sense_eeg.git
 ```
@@ -27,4 +26,8 @@ On a different terminal, run
 source install/setup.bash
 ros2 topic list
 ros2 topic echo /eeg/raw
+ros2 bag record /eeg/raw
+ros2 bag info <bag_file_name> (e.g., `rosbag2_data_time`)
+ros2 bag play <bag_file_name>
+rqt_graph
 ```
--- src/eeg_publisher/eeg_publisher/mock_publisher.py
+++ src/eeg_publisher/eeg_publisher/mock_publisher.py
@@ -5,23 +5,36 @@ import numpy as np
 from std_msgs.msg import Header
 from eeg_msgs.msg import EEGBlock
 
+
 class MockEEGPublisher(Node):
-    def __init__(self):
+    def __init__(self, ):
         super().__init__('mock_eeg_publisher')
-        self.publisher = self.create_publisher(EEGBlock, '/eeg/raw', 10)
-        self.timer = self.create_timer(0.125, self.publish_data)  # 8Hz (32 samples at 256Hz)
-        self.get_logger().info('Mock EEG Publisher running...')
-    
+        np.random.seed(0)
+        self.queue_size = 10
+        self.num_channels = 8
+        self.num_samples = 32
+        self.sampling_rate = 256.0
+        self.timer_period_in_secs = 1 / (self.sampling_rate/self.num_samples)
+        self.publisher = self.create_publisher(EEGBlock, '/eeg/raw', self.queue_size)
+        self.timer = self.create_timer(self.timer_period_in_secs, self.publish_data)
+        self.get_logger().info(
+            f'Mock EEG Publisher running at {self.sampling_rate} Hz '
+            f'with {self.num_samples} samples and '
+            f'timer period of {self.timer_period_in_secs} seconds'
+        )
+
+
     def publish_data(self):
         msg = EEGBlock()
         msg.header = Header()
         msg.header.stamp = self.get_clock().now().to_msg()
-        msg.num_channels = 8
-        msg.num_samples = 32
-        msg.sampling_rate = 256.0
+        msg.num_channels = self.num_channels
+        msg.num_samples = self.num_samples
+        msg.sampling_rate = self.sampling_rate
         msg.data = np.random.randn(msg.num_channels * msg.num_samples).astype(np.float32).tolist()
         self.publisher.publish(msg)
         self.get_logger().info(f'Published EEGBlock: {len(msg.data)} samples')
+
 
 def main(args=None):
     rclpy.init(args=args)
