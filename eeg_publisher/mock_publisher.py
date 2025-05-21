a68089fae3105c90daa894b56c7c2d8549e3027a
Stephen Thompson <s.thompson@ucl.ac.uk>
2025-05-21 13:32:30 +0100

    Trying launch configuration for mock publisher

--- eeg_publisher/mock_publisher.py
+++ eeg_publisher/mock_publisher.py
@@ -17,34 +17,44 @@ class MockEEGPublisher(Node):  # type: ignore[misc]
 
         self.queue_size: int = 10
         self.num_channels: int = 8
-        self.num_samples: int = 32
-        self.sampling_rate: float = 256.0
+        self.declare_parameter('num_channels', 8)
+        self.declare_parameter('num_samples', 32)
+        self.declare_parameter('sampling_rate', 256.0)
 
         self.rng = default_rng(self.n_seed)
 
-        self.timer_period_in_secs: float = 1 / (self.sampling_rate / self.num_samples)
+        sample_rate = self.get_parameter('sampling_rate')
+        n_samples = self.get_parameter('num_samples')
+
+        timer_period_in_secs: float = 1 / (sample_rate / n_samples)
 
         self.publisher: Publisher = self.create_publisher(
             EEGBlock, "/eeg/raw", self.queue_size
         )
-
+        
+        # this might be problematic if we change sample_rate or n_samples on the 
+        # fly, as the timer period won't be recalculated
         self.timer: Timer = self.create_timer(
-            self.timer_period_in_secs, self.publish_data
+            timer_period_in_secs, self.publish_data
         )
 
         self.get_logger().info(
-            f"Mock EEG Publisher running at {self.sampling_rate} Hz "
-            f"with {self.num_samples} samples and "
-            f"timer period of {self.timer_period_in_secs} seconds"
+            f"Mock EEG Publisher running at {sample_rate} Hz "
+            f"with {n_samples} samples and "
+            f"timer period of {timer_period_in_secs} seconds"
         )
 
     def publish_data(self) -> None:
+        n_channels = self.get_parameter('num_channels').get_parameter_value().integer_value
+        sample_rate = self.get_parameter('sampling_rate').get_parameter_value().float_value
+        n_samples = self.get_parameter('num_samples').get_parameter_value().integer_value
         msg = EEGBlock()
+
         msg.header = Header()
         msg.header.stamp = self.get_clock().now().to_msg()
-        msg.num_channels = self.num_channels
-        msg.num_samples = self.num_samples
-        msg.sampling_rate = self.sampling_rate
+        msg.num_channels = n_channels
+        msg.num_samples = n_samples
+        msg.sampling_rate = sample_rate
         msg.data = (
             self.rng.standard_normal(msg.num_channels * msg.num_samples)
             .astype(np.float32)
--- launch/mock_publisher_launch.py
+++ launch/mock_publisher_launch.py
@@ -1,5 +1,7 @@
 from launch import LaunchDescription  # type: ignore
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description() -> LaunchDescription:
@@ -10,6 +12,9 @@ def generate_launch_description() -> LaunchDescription:
                 namespace="eeg_publisher1",
                 executable="mock_publisher",
                 name="mock_publisher",
+                parameters=[PathJoinSubstitution([
+                FindPackageShare('eeg_publisher'), 'config', 'mock_publisher.yaml'])
+                ],
             )
         ]
     )
